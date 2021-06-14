414d30f5893f55fe8447dd74c1cb9876ea4fbc8b
zhengted <zhengted@163.com>
2021-06-14 23:20:28 +0800

    add separate gold

--- Zuo/Greedy.go
+++ Zuo/Greedy.go
@@ -1,6 +1,9 @@
 package Zuo
 
-import "math"
+import (
+	"container/heap"
+	"math"
+)
 
 // 会议安排问题
 // 给定一个meeting数组，求24h内能安排的最多的meeting数
@@ -80,7 +83,57 @@ func lightContain(light []int, index int) bool {
 
 // 金条分割
 //  给定数组{10,20,30}表示三个人想要的金条数  金条长度60
-//   1. 先分成
+//   1. 堆存储
+//   2. 每次弹出最小的两个 合并并放入堆中
+type GoldenHeap []int
+
+func (h GoldenHeap) Len() int {
+	return len(h)
+}
+
+func (h GoldenHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h GoldenHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *GoldenHeap) Push(val interface{}) {
+	*h = append(*h, val.(int))
+}
+
+func (h *GoldenHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	old = old[:n-1]
+	*h = old
+	return x
+}
+
+func SeparateGolden(arr []int) int {
+	if len(arr) < 1 {
+		return 0
+	}
+	if len(arr) == 1 {
+		return arr[0]
+	}
+	h := &GoldenHeap{}
+	heap.Init(h)
+	for _, n := range arr {
+		heap.Push(h, n)
+	}
+	ret := 0
+	for len(*h) > 1 {
+		n1 := heap.Pop(h).(int)
+		n2 := heap.Pop(h).(int)
+		n3 := n1 + n2
+		ret += n3
+		heap.Push(h, n3)
+	}
+	return ret
+}
 
 func minInt(a, b int) int {
 	if a < b {
